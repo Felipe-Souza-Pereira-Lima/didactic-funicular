b8ecb5fd913449a957c9b82e7d4a18a3cc6a826b
Felipe Souza Pereira Lima <eersc12@gmail.com>
2020-12-28 15:47:19 -0300

    Update Main-Codec.ino

--- Main-Codec/Main-Codec.ino
+++ Main-Codec/Main-Codec.ino
@@ -1,9 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
 
+void setup() {
+  // Configurações
+  Serial.begin(9600);
+  
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(12, HIGH); 
+  delay(5000);
+  digitalWrite(12, LOW); 
+  delay(1000);
 }
