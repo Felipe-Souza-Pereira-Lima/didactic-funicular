cc0d26957afa8cd5b65a49757e065778ce19b3e6
Felipe Souza Pereira Lima <eersc12@gmail.com>
2020-12-28 16:04:37 -0300

    Update Main-Codec.ino

--- Main-Codec/Main-Codec.ino
+++ Main-Codec/Main-Codec.ino
@@ -1,15 +1,30 @@
+#include <Ethernet.h>
+char code = ""
+int local = 0
 
 void setup() {
-  // Configurações
-  Serial.begin(9600);
-  
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
+    // Configurações
+    Serial.begin(9600);
+    
+    pinMode(13, OUTPUT);
+    pinMode(12, OUTPUT);
+    pinMode(11, INPUT);
 }
 
-void loop() {
-  digitalWrite(12, HIGH); 
-  delay(5000);
-  digitalWrite(12, LOW); 
-  delay(1000);
+void loop() {  
+    // Acionamento do hardware A
+    if digitalWrite(11) == HIGH {
+          digitalWrite(12, HIGH);
+          Serial.println("GENIUS: Hardware (A) foi acionado com sucesso!")
+          delay(9600);
+          digitalWrite(12, LOW);
+     }
+     // Leitura do hospedeiro
+     if analogWrite(0) == HIGH {
+          Serial.println("------------------------------------------------------------")
+          Serial.println("GENIUS: O HOSPEDEIRO reconheceu um valor de ENTRADA         ")
+          Serial.println("GENIUS: Para ejetar a o valor RESET a placa ou insira outro ")
+          Serial.println("------------------------------------------------------------")
+     }
+     
 }
