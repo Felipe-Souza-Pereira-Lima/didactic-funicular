1330f7b0140b311fcbd379370d0a7423027562e3
Felipe Souza Pereira Lima <eersc12@gmail.com>
2021-01-01 12:20:42 -0300

    Update Main-Codec.ino

--- Main-Codec/Main-Codec.ino
+++ Main-Codec/Main-Codec.ino
@@ -1,33 +1,10 @@
-#include <Ethernet.h>
-#include <SD.h> 
-
-char code = "ETHERNET.h";
-int local = 0;
-
 void setup() {
-    // Configurações
     Serial.begin(9600);
-    
+    Serial.println("Iniciando GENIUS v0.0.1");
+        
     pinMode(13, OUTPUT);
     pinMode(12, OUTPUT);
-    pinMode(11, INPUT);
-    // Leitura SD and Starts DRIVERS
-    Serial.print("Iniciando DRIVERS...");
-
-    if (!SD.begin(4)) {
-      Serial.println("Inicialização falhou!");
-      local = 1;
-      while (1);
-    }
-    
-    char myFile = Serial;
-  
-    if (myFile) {
-        Serial.print("Caregando o API-EXECUTE.exe...");
-        Serial.println("Concluido.");
-    } else {
-        Serial.println("Erro ao abrir API-EXECUTE.exe");
-    }
+    pinMode(11, INPUT);    
 }
 
 void loop() {  
